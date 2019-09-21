7fe26a8263e2cbdfbe522f89826fff7a412e5309
HCS Learning <learning@hardcybersoft.cl>
2019-09-20 21:24:35 -0300

    Comando funcionando y guardando en BD SQLite

--- src/Command/TareasCronCommand.php
+++ src/Command/TareasCronCommand.php
@@ -8,22 +8,32 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Tarea;
 
-class TareasCronCommand extends Command
-{
+class TareasCronCommand extends Command {
+
     protected static $defaultName = 'app:tareas-cron';
+    private $em;
 
-    protected function configure()
-    {
+    function __construct(EntityManagerInterface $em) {
+        parent::__construct();
+        $this->em = $em;
+    }
+
+    private function getEm(): EntityManagerInterface {
+        return $this->em;
+    }
+
+    protected function configure() {
         $this
-            ->setDescription('Ejecuta todas las tareas de mantención para programar vía CRON')
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+                ->setDescription('Ejecuta todas las tareas de mantención para programar vía CRON')
+                ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+                ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
         ;
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output)
-    {
+    protected function execute(InputInterface $input, OutputInterface $output) {
         $io = new SymfonyStyle($input, $output);
         $arg1 = $input->getArgument('arg1');
 
@@ -35,6 +45,14 @@ class TareasCronCommand extends Command
             // ...
         }
 
+        $tarea = (new Tarea())
+                ->setNombre("prueba")
+                ->setComentario("ejecución: " . (new \DateTime('now'))->format('Y-m-d H:i:s'))
+        ;
+        $this->getEm()->persist($tarea);
+        $this->getEm()->flush();
+
         $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
     }
+
 }
---
+++ src/Entity/Tarea.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TareaRepository")
+ */
+class Tarea
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $comentario;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getComentario(): ?string
+    {
+        return $this->comentario;
+    }
+
+    public function setComentario(string $comentario): self
+    {
+        $this->comentario = $comentario;
+
+        return $this;
+    }
+}
